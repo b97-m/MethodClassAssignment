aa2335ba727852b4e4e1a0a71d3588ff818e8037
b97-m <lee.meymon@gmail.com>
2023-12-09 19:26:41 +0000

    Add project files.

---
+++ MethodClassAssignment/MathOperation1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class MathOperation1
+    {
+        //A method that takes 2 integers as parameters
+        //One of them is optional by providing a default value
+        public int MathOperation(int i, int j = 1)
+        {
+            //Multiplication on integers and the result
+            int result = i * j;
+            return result;
+        }
+    }
+}
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate MathOperation1 clss
+            MathOperation1 mathOp = new MathOperation1();
+
+            //Ask the user to provide the first number
+            Console.WriteLine("Please enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Ask the user to provide a second number or press enter
+            Console.WriteLine("Please enter a second integer, or press enter: ");
+            try
+            {
+                //Call a method with 2 parameters if both parameters are provided
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = mathOp.MathOperation(num1, num2);
+                Console.WriteLine(num1 + " x " + num2 + " = " + results);
+            }
+            catch
+            {
+                //Call a method with one parameter if only one is provied by the user
+                int results = mathOp.MathOperation(num1);
+                Console.WriteLine(num1 + " x Default 1 = " + results);
+            }
+            Console.ReadLine();           
+         
+        }
+    }
+}
