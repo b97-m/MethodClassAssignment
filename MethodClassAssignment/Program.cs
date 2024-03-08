a57319b878884e312e19e4c743850b67f4be67e5
b97-m <102753461+b97-m@users.noreply.github.com>
2024-03-08 23:33:49 +0000

    Update Program.cs

--- MethodClassAssignment/Program.cs
+++ MethodClassAssignment/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MethodClassAssignment
 {
@@ -6,30 +6,14 @@ namespace MethodClassAssignment
     {
         static void Main(string[] args)
         {
-            //Instantiate MathOperation1 clss
-            MathOperation1 mathOp = new MathOperation1();
+            // Instantiate the class
+            MathOperation operation = new MathOperation();
 
-            //Ask the user to provide the first number
-            Console.WriteLine("Please enter an integer: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            // Call the method by passing in two numbers 
+            operation.PerformOperation(5, 10);
 
-            //Ask the user to provide a second number or press enter
-            Console.WriteLine("Please enter a second integer, or press enter: ");
-            try
-            {
-                //Call a method with 2 parameters if both parameters are provided
-                int num2 = Convert.ToInt32(Console.ReadLine());
-                int results = mathOp.MathOperation(num1, num2);
-                Console.WriteLine(num1 + " x " + num2 + " = " + results);
-            }
-            catch
-            {
-                //Call a method with one parameter if only one is provied by the user
-                int results = mathOp.MathOperation(num1);
-                Console.WriteLine(num1 + " x Default 1 = " + results);
-            }
-            Console.ReadLine();           
-         
+            // Call the method by specifying the parameters by name
+            operation.PerformOperation(firstNumber: 8, secondNumber: 20);
         }
     }
 }
